f358a98a50d1eefa3c70fdd192835919d8315960
Matthew Lewis <matthew.knight.lewis@gmail.com>
2020-10-22 16:24:24 -0400

    Updates

---
+++ precache-manifest.7773b84a4dc1511fd0463b091d41e9b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6006d6bab81523dc6ad24643d11d635f",
+    "url": "/react-ui-api/index.html"
+  },
+  {
+    "revision": "be92725e844f89e03b02",
+    "url": "/react-ui-api/static/css/main.6aee9bd6.chunk.css"
+  },
+  {
+    "revision": "86e1afaa93d0caddfcff",
+    "url": "/react-ui-api/static/js/2.f86f8fdc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-ui-api/static/js/2.f86f8fdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be92725e844f89e03b02",
+    "url": "/react-ui-api/static/js/main.ed25b5a5.chunk.js"
+  },
+  {
+    "revision": "1c9c72596d2c5cd783ce",
+    "url": "/react-ui-api/static/js/runtime-main.139f1a8c.js"
+  }
+]);
