6070d4cf92e616183b83157a0b275d2bd42bf767
Matthew Lewis <matthew.knight.lewis@gmail.com>
2020-10-22 15:55:56 -0400

    Updates

---
+++ precache-manifest.21084fe0df20abaeec64086be64b3778.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d16d34d38a7909fa7003c391f329ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "679739230dbec186474f",
+    "url": "/static/css/main.6aee9bd6.chunk.css"
+  },
+  {
+    "revision": "86e1afaa93d0caddfcff",
+    "url": "/static/js/2.f86f8fdc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.f86f8fdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "679739230dbec186474f",
+    "url": "/static/js/main.d1083282.chunk.js"
+  },
+  {
+    "revision": "665dd62e7605a7fe052f",
+    "url": "/static/js/runtime-main.8d3b78ed.js"
+  }
+]);
